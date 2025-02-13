6c4b2bac4ec468ee6fbdd74aca9b8d18f6731dd7
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-13 18:58:47 +0100

    Create Program.cs

---
+++ MiniORM.App/Program.cs
@@ -0,0 +1,18 @@
+using MiniORM.App;
+using MiniORM.App.Entities;
+
+const string connectionString = "Server=.;Database=MiniORM;User ID=youruser;Password=yourpassword;TrustServerCertificate=True";
+var dbContext = new SoftUniDbContext(connectionString);
+
+// Add
+// dbContext.Projects.Add(new Project { Name = "MiniORM" });
+
+// Remove
+var projectToDelete = dbContext.Projects.FirstOrDefault(x => x.Name == "MiniORM");
+if (projectToDelete is not null) dbContext.Projects.Remove(projectToDelete);
+
+// Update
+var employeesToUpdate = dbContext.Employees.Where(e => e.LastName == "Taylor" || !e.IsEmployed).ToList();
+foreach (var employee in employeesToUpdate) employee.LastName = "Smith";
+
+dbContext.SaveChanges();
