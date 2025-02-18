c64b4b2e0a2a02c810fdd428fcdf4417dce194e2
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-18 01:44:59 +0100

    Update ReflectionHelper.cs

--- MiniORM/ReflectionHelper.cs
+++ MiniORM/ReflectionHelper.cs
@@ -5,10 +5,7 @@ using System.Reflection;
 
 internal static class ReflectionHelper
 {
-    /// <summary>
-    /// Replaces an auto-generated backing field with an object instance.
-    /// Commonly used to set properties without a setter.
-    /// </summary>
+  
     public static void ReplaceBackingField(object sourceObj, string propertyName, object targetObj)
     {
         var backingField = sourceObj.GetType()
@@ -18,9 +15,7 @@ internal static class ReflectionHelper
         backingField.SetValue(sourceObj, targetObj);
     }
 
-    /// <summary>
-    /// Extension method for MemberInfo, which checks if a member contains an attribute.
-    /// </summary>
+  
     public static bool HasAttribute<T>(this MemberInfo mi)
         where T : Attribute
     {
