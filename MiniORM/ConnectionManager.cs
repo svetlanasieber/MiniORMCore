524b1bd9c469f405987efe95ee7140649abd67d6
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-02-18 01:44:10 +0100

    Update ConnectionManager.cs

--- MiniORM/ConnectionManager.cs
+++ MiniORM/ConnectionManager.cs
@@ -1,9 +1,5 @@
 ﻿namespace MiniORM;
 
-/// <summary>
-/// Used for wrapping a database connection with a using statement and
-/// automatically closing it when the using statement ends
-/// </summary>
 internal class ConnectionManager : IDisposable
 {
     private readonly DatabaseConnection _connection;
